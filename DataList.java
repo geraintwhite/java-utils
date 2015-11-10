e60db2cda8ae85a61caa384bafecdaeb46a5ccb0
Geraint White <geraintwhite@gmail.com>
2015-11-10 12:19:24 +0000

    Add reverse method to DataList

--- DataList.java
+++ DataList.java
@@ -7,8 +7,8 @@ import Utils.Matcher;
 import Utils.DataItem;
 
 public class DataList<E extends DataItem> extends ArrayList<E> {
-  public ArrayList<E> filter(Matcher<E> matcher) {
-    ArrayList<E> list = new ArrayList<E>();
+  public DataList<E> filter(Matcher<E> matcher) {
+    DataList<E> list = new DataList<E>();
     for (E element : this) {
       if (matcher.match(element)) {
         list.add(element);
@@ -17,7 +17,7 @@ public class DataList<E extends DataItem> extends ArrayList<E> {
     return list;
   }
 
-  public <T> ArrayList<E> filterByParameter(final String parameter, final T value) {
+  public <T> DataList<E> filterByParameter(final String parameter, final T value) {
     return this.filter(new Matcher<E>() {
       public Boolean match(E e) {
         return e.get(parameter).equals(value);
@@ -25,11 +25,17 @@ public class DataList<E extends DataItem> extends ArrayList<E> {
     });
   }
 
-  public void sort(final String parameter) {
+  public DataList<E> sort(final String parameter) {
     Collections.sort(this, new Comparator<E>() {
       public int compare(E e1, E e2) {
         return e1.get(parameter).compareTo(e2.get(parameter));
       }
     });
+    return this;
+  }
+
+  public DataList<E> reverse() {
+    Collections.reverse(this);
+    return this;
   }
 }
