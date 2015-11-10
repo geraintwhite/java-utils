277ceff3452314b1b54199d81b88fc145873931c
Geraint White <geraintwhite@gmail.com>
2015-11-10 21:00:00 +0000

    Add DataItem constructor

--- DataItem.java
+++ DataItem.java
@@ -5,6 +5,10 @@ import java.util.HashMap;
 public abstract class DataItem {
   protected HashMap<String, Comparable> map;
 
+  protected DataItem() {
+    this.map = new HashMap<String, Comparable>();
+  }
+
   public Comparable get(String parameter) {
     return this.map.get(parameter);
   }
